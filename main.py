8a084da863a0a8380c1d2fe0eb727f954739ea4b
idenc <iden.craven@gmail.com>
2019-02-28 08:50:50 -0700

    Began courses

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def home():
     cur = mysql.connection.cursor()
     letter = request.args.get('letter')
     if letter is None:
-        query = "SELECT dep_name FROM department"
+        query = "SELECT dep_name, dep_code FROM department"
     else:
-        query = "SELECT dep_name FROM department WHERE dep_name LIKE '{0}%'".format(letter)
+        query = "SELECT dep_name, dep_code FROM department WHERE dep_name LIKE '{0}%'".format(letter)
 
     cur.execute(query)
     dept_details = cur.fetchall()
@@ -29,6 +29,18 @@ def home():
     for c in dept_details:
         alphabet.add(c[0][0].upper())
     return render_template('index.html', alphabet=sorted(alphabet), depts=dept_details)
+
+
+@app.route('/courses')
+def courses():
+    cur = mysql.connection.cursor()
+    id = request.args.get('id')
+    cur.execute("SELECT dep_name FROM department WHERE dep_code = %s", (id))
+    name = cur.fetchone()[0]
+
+    cur.execute("SELECT * FROM course WHERE dep_code = %s", (id))
+    courses = cur.fetchall()
+    return render_template('courses.html', name=name, courses=courses)
 
 
 if __name__ == '__main__':
